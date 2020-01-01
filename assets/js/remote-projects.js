e550b469b1c16a04c66951656d216fa80c96aef5
Tiffany White <hey@tiffanyrwhite.com>
2019-12-31 23:32:26 -0500

    Update something

--- assets/js/remote-projects.js
+++ assets/js/remote-projects.js
@@ -2,7 +2,7 @@
 ---
 
 function get_tools(repo_name) {
-  fetch(`https://api.github.com/repos/twhite96/${repo_name}/topics`, {
+  fetch(`https://api.github.com/repos/{{ site.github.owner_name }}/${repo_name}/topics`, {
     headers: {
       Accept: "application/vnd.github.mercy-preview+json"
     }
