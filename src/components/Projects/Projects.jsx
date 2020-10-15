72a16e4264442c4db5e99e277b7049cd187b797a
Tiffany White <twhite96@users.noreply.github.com>
2020-10-15 01:24:28 -0400

    Remove ellipses from button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -55,7 +55,7 @@ const Projects = () => {
                         className="cta-btn cta-btn--hero"
                         href={url || '#!'}
                       >
-                        See More...
+                        See More
                       </a>
 
                       {repo && (
