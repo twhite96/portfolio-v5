fd98acf38e8de40b413d2a67ffb86fa104db77a9
Tiffany White <hey@tiffanyrwhite.com>
2021-01-28 22:06:36 -0500

    Update something

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
