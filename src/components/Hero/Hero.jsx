cd96f4be9a83d7ebe1e5c1fd28634e60aca39f8b
Tiffany White <hey@tiffanyrwhite.com>
2021-03-05 00:27:54 -0500

    Update something

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -40,7 +40,7 @@ const Header = () => {
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta}
               </Link>
             </span>
           </p>
